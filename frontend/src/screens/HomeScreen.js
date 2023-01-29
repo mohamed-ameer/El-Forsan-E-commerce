3781722820e4056bb434e7a62f48033c37710c7c
mohamed ameer <mohamedameer577@gmail.com>
2023-01-30 00:57:42 +0200

    30/1/2023

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,37 @@
 import React,{useState,useEffect} from 'react'
-import axios from 'axios'
 import { Row, Col } from 'react-bootstrap'
 import { useTranslation } from "react-i18next";
 import Product from '../components/Product/Product'
+import Loader from '../components/Loader/Loader'
+import Message from '../components/Message/Message'
 import Slider from '../components/Slider/Slider'
+import { useDispatch,useSelector } from 'react-redux';
+import {listProducts} from '../actions/productActions'
 function HomeScreen() {
     const [t,i18n]=useTranslation();
-    const [products,setProducts]=useState([]);
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList)
+    const {error,products,loading} = productList
     useEffect(()=>{
-        const url = 'http://127.0.0.1:8000/api/products/'
-        axios.get(url).then((response)=>{
-            setProducts(response.data)
-        })
+        dispatch(listProducts())
     },[])
-  return (
+
+    return (
     <>
         <div className='container pb-5 pt-0' style={{maxWidth:"800px"}}>
             <Slider/>
         </div>
-        <Row style={i18n.language == 'ar'?{flexDirection:'row-reverse'}:{flexDirection:'row'}}>
-        {products.map(product => (
-            <Col key={product._id} sm={6} md={6} lg={4} xl={3}>
-                <Product product={product} />
-            </Col>
-        ))}
-        </Row>
+        {loading ? <Loader />
+            :error ? <Message variant='danger'>{error}</Message>
+                :
+                <Row style={i18n.language == 'ar'?{flexDirection:'row-reverse'}:{flexDirection:'row'}}>
+                {products.map(product => (
+                    <Col key={product._id} sm={6} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+                </Row>
+        }
     </>
   )
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,101 +1,111 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
 import { Link } from 'react-router-dom'
 import { useTranslation } from "react-i18next";
 import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating/Rating'
+import Loader from '../components/Loader/Loader'
+import Message from '../components/Message/Message'
+import { useDispatch,useSelector } from 'react-redux'
+import { listProductDetails } from '../actions/productActions'
 import { useParams } from 'react-router-dom';
 
 function ProductScreen() {
   const { id } = useParams()
   const [t,i18n]=useTranslation();
-  const [product,setProduct]=useState({})
+  const dispatch = useDispatch();
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product } = productDetails
   useEffect(()=>{
-    const url = `http://127.0.0.1:8000/api/products/${id}/`
-    axios.get(url).then((response)=>{
-        setProduct(response.data)
-    })
-  },[id])
+    dispatch(listProductDetails(id))
+  },[dispatch,id])
   return (
-    <div style={i18n.language == 'ar'?{direction:'rtl'}:{direction:'ltr'}}> 
-      <nav aria-label="breadcrumb" className="mb-4">
-        <ol className="breadcrumb" style={{direction:'ltr',backgroundColor:'#ddd',padding:'10px 15px',marginBottom:'0px',borderRadius:'8px'}}>
-            <li className="breadcrumb-item "><Link to='/'>{i18n.language == 'ar'?'الصفحه الرئيسيه':'Home'}</Link></li>
-            <li className="breadcrumb-item active " aria-current="page">{i18n.language == 'ar'?product.name_ar:product.name}</li>
-        </ol>
-      </nav> 
-      <Row>
-          <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
-          </Col>
+    <>
+        { loading ?
+            <Loader />
+            : error 
+                ? <Message variant='danger'>{error}</Message>
+                :
+                <div style={i18n.language == 'ar'?{direction:'rtl'}:{direction:'ltr'}}> 
+                <nav aria-label="breadcrumb" className="mb-4">
+                    <ol className="breadcrumb" style={{direction:'ltr',backgroundColor:'#ddd',padding:'10px 15px',marginBottom:'0px',borderRadius:'8px'}}>
+                        <li className="breadcrumb-item "><Link to='/'>{i18n.language == 'ar'?'الصفحه الرئيسيه':'Home'}</Link></li>
+                        <li className="breadcrumb-item active " aria-current="page">{i18n.language == 'ar'?product.name_ar:product.name}</li>
+                    </ol>
+                </nav> 
+                <Row>
+                    <Col md={6}>
+                        <Image src={product.image} alt={product.name} fluid />
+                    </Col>
 
-          <Col md={6}>
-              <ListGroup variant="flush">
-                  <ListGroup.Item>
-                      <h3>{i18n.language == 'ar'?product.name_ar:product.name}</h3>
-                  </ListGroup.Item>
+                    <Col md={6}>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <h3>{i18n.language == 'ar'?product.name_ar:product.name}</h3>
+                            </ListGroup.Item>
 
-                  <ListGroup.Item style={{direction:'ltr'}}>
-                      <Rating value={product.rating} text={`${product.numReviews}`} color={'#f8e825'}/>
-                  </ListGroup.Item>
+                            <ListGroup.Item style={{direction:'ltr'}}>
+                                <Rating value={product.rating} text={`${product.numReviews}`} color={'#f8e825'}/>
+                            </ListGroup.Item>
 
-                  <ListGroup.Item>
-                      <strong>{i18n.language == 'ar'?'السعر :':'Price :'}</strong> {i18n.language == 'ar' ?`${product.price} جنيه`:`${product.price} EGP`}
-                  </ListGroup.Item>
-                  {i18n.language == 'ar' &&
-                  <ListGroup.Item className={product.countInStock > 0 ? 'text-white bg-success' : 'text-white bg-danger'}>
-                      <strong>الحاله :</strong> {product.countInStock > 0 ? 'متوفر' : 'غير متوفر حاليا'}
-                  </ListGroup.Item>
-                    }
-                  {i18n.language == 'en' &&
-                  <ListGroup.Item className={product.countInStock > 0 ? 'text-white bg-success' : 'text-white bg-danger'}>
-                      <strong>Status:</strong> {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                  </ListGroup.Item>
-                    }
-                  <ListGroup.Item>
-                      <strong>{i18n.language == 'ar'?'المكونات :':'Ingredient'}</strong> {i18n.language == 'ar'?product.description_ar:product.description}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                      <strong>{i18n.language == 'ar'?'الوزن :':'Weight :'}</strong> {product.weight} {i18n.language == 'ar'?'كجم':'kg'}
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                      <strong>{i18n.language == 'ar'?'الوزن الصافي :':'Package Weight :'}</strong> {product.package_weight} {i18n.language == 'ar'?'جم':'g'}
-                  </ListGroup.Item>
-                  {product.countInStock > 0 && (
-                    <ListGroup.Item>
-                        <Row className='align-items-center'>
-                            <Col><strong>{i18n.language == 'ar'?'الكميه :':'Quantity:'}</strong></Col>
-                            <Col xs='auto' className='my-1'>
-                                <Form.Control
-                                    as="select"
-                                >
-                                    {
+                            <ListGroup.Item>
+                                <strong>{i18n.language == 'ar'?'السعر :':'Price :'}</strong> {i18n.language == 'ar' ?`${product.price} جنيه`:`${product.price} EGP`}
+                            </ListGroup.Item>
+                            {i18n.language == 'ar' &&
+                            <ListGroup.Item className={product.countInStock > 0 ? 'text-white bg-success' : 'text-white bg-danger'}>
+                                <strong>الحاله :</strong> {product.countInStock > 0 ? 'متوفر' : 'غير متوفر حاليا'}
+                            </ListGroup.Item>
+                                }
+                            {i18n.language == 'en' &&
+                            <ListGroup.Item className={product.countInStock > 0 ? 'text-white bg-success' : 'text-white bg-danger'}>
+                                <strong>Status:</strong> {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                            </ListGroup.Item>
+                                }
+                            <ListGroup.Item>
+                                <strong>{i18n.language == 'ar'?'المكونات :':'Ingredient'}</strong> {i18n.language == 'ar'?product.description_ar:product.description}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>{i18n.language == 'ar'?'الوزن :':'Weight :'}</strong> {product.weight} {i18n.language == 'ar'?'كجم':'kg'}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <strong>{i18n.language == 'ar'?'الوزن الصافي :':'Package Weight :'}</strong> {product.package_weight} {i18n.language == 'ar'?'جم':'g'}
+                            </ListGroup.Item>
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row className='align-items-center'>
+                                        <Col><strong>{i18n.language == 'ar'?'الكميه :':'Quantity:'}</strong></Col>
+                                        <Col xs='auto' className='my-1'>
+                                            <Form.Control
+                                                as="select"
+                                            >
+                                                {
 
-                                        [...Array(product.countInStock).keys()].map((x) => (
-                                            <option key={x + 1} value={x + 1}>
-                                                {x + 1}
-                                            </option>
-                                        ))
-                                    }
+                                                    [...Array(product.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>
+                                                            {x + 1}
+                                                        </option>
+                                                    ))
+                                                }
 
-                                </Form.Control>
-                            </Col>
-                        </Row>
-                    </ListGroup.Item>
-                  )}
-                    <ListGroup.Item>
-                          <Button
-                              className='btn btn-dark btn-lg w-100'
-                              disabled={product.countInStock == 0}
-                              type='button'>
-                              {i18n.language == 'ar'?'أضف إلي السله':'Add to Cart'}
-                          </Button>
-                    </ListGroup.Item>  
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
+                                <ListGroup.Item>
+                                    <Button
+                                        className='btn btn-dark btn-lg w-100'
+                                        disabled={product.countInStock == 0}
+                                        type='button'>
+                                        {i18n.language == 'ar'?'أضف إلي السله':'Add to Cart'}
+                                    </Button>
+                                </ListGroup.Item>  
 
-              </ListGroup>
-          </Col>
-      </Row>
-    </div>
+                        </ListGroup>
+                    </Col>
+                </Row>
+                </div>
+        }
+    </>
   )
 }
 
